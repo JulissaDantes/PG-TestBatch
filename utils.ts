c039a84fd81ba8eeec58146557e4a72fe0840492
JulissaDantes <julissadcj@gmail.com>
2024-04-10 10:01:48 -0400

    flooring result

--- utils.ts
+++ utils.ts
@@ -21,7 +21,7 @@ function generateTestData(numRecords: number) {
     return testData;
 }
 
-export async function insertRecordsInChunks(client: Client, records: Record[]): Promise<{ record: Record; error: any }[]> {
+export async function insertRecordsInChunks(client: Client, records: Record[]): Promise<any[]> {
   const chunkSize = 10000;
   const errors: { record: Record; error: any }[] = [];
   
@@ -38,7 +38,7 @@ export async function insertRecordsInChunks(client: Client, records: Record[]): 
           record.email,
         ]);
       }
-      console.log("Finished chunk #:", records.length/ chunkSize);
+      console.log("Finished chunk #:", Math.floor(((i + 1)/chunkSize) + 1)," of ", records.length/ chunkSize);
       await client.query('COMMIT');
       console.log("Committed")
     } catch (error) {
@@ -46,8 +46,37 @@ export async function insertRecordsInChunks(client: Client, records: Record[]): 
       errors.push(...chunk.map((record) => ({ record, error })));
     }
   }
+  return errors
+}
 
-  return errors;
+  export async function readRecordsInChunks(client: Client, records: Record[]): Promise<any[]> {
+    const chunkSize = 10000;
+    const errors: { record: Record; error: any }[] = [];
+    const res = [];
+    
+    for (let i = 0; i < records.length; i += chunkSize) {
+      console.log("about to start at:", i);
+      const chunk = records.slice(i, i + chunkSize);
+      try {
+        await client.query('BEGIN');
+  
+        for (const record of chunk) {
+          res.push(await client.query('Select * from your_table where name = $1 and address= $2 and email = $3', [
+            record.name,
+            record.address,
+            record.email,
+          ]));
+        }
+        console.log("Finished chunk #:", Math.floor(((i + 1)/chunkSize) + 1)," of ", records.length/ chunkSize);
+        await client.query('COMMIT');
+        console.log("Committed")
+      } catch (error) {
+        await client.query('ROLLBACK');
+        errors.push(...chunk.map((record) => ({ record, error })));
+      }
+    }
+
+  return res.length > 0? res : errors;
 }
 
 export async function clearDB(client: Client) {
@@ -76,8 +105,9 @@ async function main() {
     await client.connect();
 
     const records = generateTestData(1000000);
-    //const errors = await insertRecordsInChunks(client, records);
-    await clearDB(client)
+    const errors = await insertRecordsInChunks(client, records);
+    //const errors = await readRecordsInChunks(client, records);
+    //await clearDB(client)
   } catch (error) {
     console.error('Error:', error);
   } finally {
